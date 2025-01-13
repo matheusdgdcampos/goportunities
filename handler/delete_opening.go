e1340f887a09eadd633a9b2cab442a6d0ee45084
Matheus da Guia de Campos <external.matheus.campos@trisor.de>
2025-01-13 16:31:30 -0300

    docs: add delete opening swagger doc

--- handler/delete_opening.go
+++ handler/delete_opening.go
@@ -9,6 +9,18 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// @BasePath /api/v1
+// @Summary Delete opening
+// @Description Delete a job opening
+// @Tags Openings
+// @Accept json
+// @Produce json
+// @Param id query string true "Opening identification"
+// @Success 200 {object} DeleteOpeningResponse
+// @Failure 400 {object} ErrorResponse
+// @Failure 404 {object} ErrorResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /opening [delete]
 func DeleteOpeningHandler(ctx *gin.Context) {
 	id := ctx.Query("id")
 	if id == "" {
