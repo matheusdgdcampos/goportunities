2bce664b64dc34de697d2c2e4dad9c0f34832554
Matheus da Guia de Campos <external.matheus.campos@trisor.de>
2025-01-13 16:26:08 -0300

    docs: create opening swagger docs

--- handler/response.go
+++ handler/response.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"fmt"
+	"goportunities/schemas"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -21,4 +22,14 @@ func sendSuccess(ctx *gin.Context, op string, data any) {
 		"message": fmt.Sprintf("operation from handler: %s successfull", op),
 		"data":    data,
 	})
+}
+
+type ErrorResponse struct {
+	Message    string `json:"message"`
+	StatusCode string `json:"statusCode"`
+}
+
+type CreateOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
 }
